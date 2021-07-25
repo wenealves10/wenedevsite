a600b110d2d160b90920af7eeec21670b5acddc2
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-25 16:28:54 -0300

    New Cards Image Techs 💯

--- components/Cover/index.tsx
+++ components/Cover/index.tsx
@@ -1,17 +1,42 @@
-import { Box, Button, Flex, Heading, Text, useColorModeValue } from '@chakra-ui/react';
-import { colors } from 'src/../styles/colors';
+import Image from 'next/image';
+import {
+  Heading,
+  Button,
+  Flex,
+  Text,
+  Box,
+  SimpleGrid,
+  useColorModeValue,
+  Wrap,
+  WrapItem,
+  Center,
+  Link,
+} from '@chakra-ui/react';
 import { ITechnologies } from 'src/../interfaces/props.dto';
+import { useState } from 'react';
 
 function Cover({ technologies }: ITechnologies){
 
-  const bgColor = useColorModeValue('#F4F6F8', '#1A202C');
+  const [currentTechnologies, setTechnologies] = useState(technologies);
+  const bgColor = useColorModeValue('#FFFFFF', '#1A202C');
+
+  const handleShowAllTechnologies = () => {
+    const techs = currentTechnologies.map((t) => {
+      t.defaultVisible = true;
+      return t;
+    });
+
+    setTechnologies(techs);
+  }
+
+  const hiddenTechnologies = currentTechnologies.filter((t) => !t.defaultVisible,).length;
 
   return (
-    <Box bgColor={bgColor} color="black">
+    <Box bgColor={bgColor}>
       <Flex
        justifyContent="center"
        alignItems="center"
-       py={20}
+      //  py={-100}
       >
         <Flex
           px={[4,8]}
@@ -41,25 +66,76 @@ function Cover({ technologies }: ITechnologies){
             </Box>
             <Box>
               tecnologias que estão disponíveis no mercado!
-              <ul>
-                {technologies.map(technology => (
-                  <li key={technology.id}>{technology.name}</li>
-                ))}
-              </ul>
             </Box>
           </Text>
           <Box>
             <Button
               as="a"
               my={10}
-              colorScheme="facebook"
-              color="purple"
+              colorScheme="purple"
               variant="outline"
               size="lg"
               href="#series"
             >
               Bora começar!
             </Button>
+          </Box>
+          <Box>
+          <Wrap>
+              {currentTechnologies
+                ?.filter((f) => f.defaultVisible)
+                ?.map((tech) => (
+                  <WrapItem>
+                    <Center
+                      w="100px"
+                      h="100px"
+                      borderWidth="1px"
+                      borderRadius="lg"
+                      overflow="hidden"
+                      flexDirection="column"
+                    >
+                      <Image
+                        src={tech.logo.url}
+                        alt={tech.name}
+                        width={40}
+                        height={40}
+                        title={tech.name}
+                      />
+                      <Text
+                        fontSize="sm"
+                        textAlign="center"
+                        fontWeight="bold"
+                        mt={2}
+                      >
+                        {tech.name}
+                      </Text>
+                    </Center>
+                  </WrapItem>
+                ))}
+              {hiddenTechnologies > 0 && (
+                <WrapItem>
+                  <Center
+                    w="100px"
+                    h="100px"
+                    borderWidth="1px"
+                    borderRadius="lg"
+                    overflow="hidden"
+                    flexDirection="column"
+                  >
+                    <Link onClick={handleShowAllTechnologies}>
+                      <Text
+                        fontSize="sm"
+                        textAlign="center"
+                        fontWeight="bold"
+                        mt={2}
+                      >
+                        {`+${hiddenTechnologies} outras`}
+                      </Text>
+                    </Link>
+                  </Center>
+                </WrapItem>
+              )}
+            </Wrap>
           </Box>
         </Flex>
       </Flex>
