6fd28261f691ee49fab0b9d48729f00048bdbd9f
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-24 21:24:59 -0300

    Get All Technologies 👍

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,29 @@
+import { ITechnologies } from 'interfaces/props.dto';
+import { GetStaticProps, GetStaticPropsContext } from 'next';
 import Cover from 'src/components/Cover';
 import Layout from 'src/components/Layout';
+import { getAllTechnologies } from 'src/lib/dato-cms';
 // import useAuth from 'src/hooks/useAuth';
 
-export default function Home() {
+export default function Home({ technologies }: ITechnologies) {
 
   // const { user, signin } = useAuth();
   // console.log("User: ", user);
   return (
    <Layout>
-     <Cover/>
+     <Cover technologies={technologies}/>
    </Layout>
   )
+}
+
+export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
+
+  const technologies = await getAllTechnologies() as ITechnologies;
+
+  return {
+    props: {
+      technologies,
+    },
+    revalidate: 120,
+  }
 }
