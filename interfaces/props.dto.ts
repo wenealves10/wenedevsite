4597984c95175f65b1d79be4d6643c080c2d06ff
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-24 21:24:24 -0300

    Interfaces 👍

--- interfaces/props.dto.ts
+++ interfaces/props.dto.ts
@@ -6,3 +6,31 @@ import { ReactNode } from 'react';
  export interface ICover {
    title?: string;
  }
+
+export interface ITechnology {
+    id: string,
+    name: string,
+    defaultVisible: boolean,
+    logo: {
+      url: string
+    }
+}
+
+export type ColorMode = "light" | "dark";
+
+export interface ITheme {
+  initialColorMode: ColorMode,
+  fonts: {
+    body: string
+  },
+  fontWeights: {
+    normal: number,
+    medium: number,
+    bold: number,
+    xBold: number,
+  }
+}
+
+ export interface ITechnologies {
+   technologies: ITechnology[],
+ }
