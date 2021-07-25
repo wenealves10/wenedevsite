d532906496e67345edd27a7da52fe4a24e9535ed
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-24 21:24:16 -0300

    Coolor Mode 👍

--- components/TopBar/index.tsx
+++ components/TopBar/index.tsx
@@ -1,11 +1,17 @@
-import { Box, Flex } from "@chakra-ui/react";
-import { colors } from 'src/../styles/colors';
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Box, Flex, useColorMode, useColorModeValue } from '@chakra-ui/react';
 
 function TopBar() {
+
+  const { colorMode, toggleColorMode } = useColorMode();
+  const bg = useColorModeValue('#FFFFFF', '#1A202C');
+  const color = useColorModeValue('#1A202C', '#EDEEEE');
+  const borderColor = useColorModeValue('#DDD', '#27272A');
+
   return (
-    <Flex bgColor={colors.bg.color2}
-    color={colors.text.color1}
-    borderBottom={`1px solid ${colors.border.color1}`}
+    <Flex bgColor={bg}
+    color={color}
+    borderBottom={`1px solid ${borderColor}`}
     w="full"
     position="fixed"
     zIndex={999}>
@@ -24,6 +30,11 @@ function TopBar() {
         <Box>
           Login
         </Box>
+        {colorMode === 'light' ? (
+          <MoonIcon w={6} h={6}  onClick={toggleColorMode}/>
+        ): (
+          <SunIcon w={6} h={6} onClick={toggleColorMode}/>
+        )}
       </Flex>
     </Flex>
   )
