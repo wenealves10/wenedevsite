2d5b025622cc0f6ff1340c0c79f75a04f3b70f02
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-25 21:27:14 -0300

    Page of Serie 👍

--- pages/serie/[slug].tsx
+++ pages/serie/[slug].tsx
@@ -1,15 +1,53 @@
+import { IAllSeries, ISerie } from 'interfaces/props.dto';
+import SerieView from 'src/components/SerieView';
+import { getAllFullSeries } from 'src/lib/dato-cms';
 import Layout from '../../components/Layout/index';
 
-function SeriePage({ serie }) {
+type Props = {
+  serie: IAllSeries;
+}
+
+function SeriePage({ serie }: Props) {
   return (
     <Layout
       title={serie.name}
       path={`/${serie.slug}`}
       description={serie.description}
     >
-
+      <SerieView serie={serie} />
     </Layout>
   )
 }
+
+export const getStaticProps = async ({ params }) => {
+  const slug = params?.slug;
+  const series = await getAllFullSeries() as IAllSeries[];
+  const serie = series.find((s) => s.slug === slug) || null;
+
+  if (!serie) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      serie,
+      allSeries: series,
+    },
+    revalidate: 60,
+  };
+};
+
+export const getStaticPaths = async () => {
+  const series = await getAllFullSeries() as IAllSeries[];
+  const slugs = series.map((s) => ({ params: { slug: s.slug } }));
+
+  return {
+    paths: slugs,
+    fallback: false,
+  };
+};
+
 
 export default SeriePage;
