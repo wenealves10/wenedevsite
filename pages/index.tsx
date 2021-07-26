694a530d4f2c439222a594aed1eb3d47aeba1b49
Wene Alves de Oliveira <weneplay5@gmail.com>
2021-07-25 21:27:45 -0300

    modify Page Index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import { GetStaticProps, GetStaticPropsContext } from 'next';
 import Layout from 'src/components/Layout';
 import Cover from 'src/components/Cover';
-import { ITechnologies } from 'interfaces/props.dto';
-import { getAllTechnologies } from 'src/lib/dato-cms';
+import { ISeries, ITechnologies } from 'interfaces/props.dto';
+import { getAllSeries, getAllTechnologies } from 'src/lib/dato-cms';
 import Footer from 'src/components/Footer';
 import { Box } from '@chakra-ui/react';
+import Series from 'src/components/Series';
 
-export default function Home({ technologies }: ITechnologies) {
+
+
+export default function Home({ technologies, series}) {
   return (
    <Layout>
      <Box pb={10}>
         <Cover technologies={technologies}/>
+        <Series series={series} />
         <Footer/>
      </Box>
    </Layout>
@@ -20,10 +24,12 @@ export default function Home({ technologies }: ITechnologies) {
 export const getStaticProps: GetStaticProps = async (context: GetStaticPropsContext) => {
 
   const technologies = await getAllTechnologies() as ITechnologies;
+  const series = await getAllSeries() as ISeries;
 
   return {
     props: {
       technologies,
+      series
     },
     revalidate: 120,
   }
