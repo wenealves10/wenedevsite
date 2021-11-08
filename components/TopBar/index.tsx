c907870507e286a4ff23029c0fc0263c75ffff71
Wene Alves De Oliveira <67342359+wenealves10@users.noreply.github.com>
2021-11-08 15:24:27 -0300

    remove wenedev

--- components/TopBar/index.tsx
+++ components/TopBar/index.tsx
@@ -49,7 +49,7 @@ function Topbar({ openAuthModal }) {
           <NextLink href="/" passHref>
             <Link>
               <Heading size="md" mr={4} display={['none', 'block']}>
-                WeneDevSite
+                Eae Dev!
               </Heading>
             </Link>
           </NextLink>
